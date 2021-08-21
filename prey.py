a5a55888d273bf2055488cd246c148ea7aa91f79
taylor <taylor.phebillo@gmail.com>
2021-08-20 20:42:14 -0500

    Add visualization, testing and bugfixes around bouncing

--- prey.py
+++ prey.py
@@ -1,4 +1,5 @@
 import random
+from PIL import Image, ImageDraw
 import math
 
 def normalize(direction, length):
@@ -8,24 +9,30 @@ def normalize(direction, length):
     return [x * length / current_len for x in direction]
 
 class Position:
-    boundaries = 1
-    acceleration_limit = 0.1
-    nearness_threshold = 0.1
+    # Cut velocity by 30% each step, leave other derivatives of position unchanged
     # TODO: elasticity = 1.0
-    def __init__(self):
+    def __init__(self, position=None, boundaries=1, acceleration_limit=0.01, nearness_threshold = 0.1, drag = [1.0, 0.7, 1.0]):
+        self.drag = drag
+        self.boundaries = boundaries
+        self.acceleration_limit = acceleration_limit
+        self.nearness_threshold = nearness_threshold
         # Position and velocity
         # TODO: Setup within boundaries?
-
-        self.position = [[random.random() for _ in range(2)] for _ in range(3)]
+        self.position = position
+        if not position:
+            # 2Dx3d- 2d for coordinates/direction, 3d means we use the third derivative
+            # to target motion by default
+            self.position = [[random.random() if deriv_level == 0 else 0 for _ in range(2)] for deriv_level in range(3)]
 
     def __repr__(self):
         return f"{self.position[0]}"
 
     def accelerate_in_direction(self, acceleration):
-        self.position[-1] = normalize(acceleration, Position.acceleration_limit)
+        self.position[-1] = normalize(acceleration, self.acceleration_limit)
         for derivative_level in range(len(self.position) - 2, -1, -1):
             for dim in range(len(self.position[0])):
                 self.position[derivative_level][dim] += self.position[derivative_level + 1][dim]
+                self.position[derivative_level][dim] *= 1.0 if derivative_level>= len(self.drag) else self.drag[derivative_level]
         self.bounce()
 
     """
@@ -36,25 +43,27 @@ class Position:
     def bounce(self):
         while self._single_bounce():
             pass
+        assert(all([self.position[0][x] >= 0 for x in range(len(self.position[0])-1)]))
+
     def _single_bounce(self):
         for i in range(len(self.position[0])):
             if self.position[0][i] < 0:
                 self._reflect(i, True)
                 return True
-            if self.position[0][i] > Position.boundaries:
+            if self.position[0][i] > self.boundaries:
                 self._reflect(i, False)
                 return True
         return False
 
     def _reflect(self, dim, low):
-        for i in range(len(self.position)):
-            if low:
-                self.position[i][dim] *= -1
-            else:
-                self.position[i][dim] -= Position.boundaries
-
+        if low:
+            self.position[0][dim] = -1 * self.position[0][dim]
+        else:
+            self.position[0][dim] -= 2*(self.position[0][dim] - self.boundaries)
+        for i in range(1, len(self.position)-1):
+            self.position[i][dim] *= -1
     def is_near(self, other_positions):
-        return min([dist(self.position[0], p.position[0]) for p in other_positions]) < Position.nearness_threshold
+        return min([dist(self.position[0], p.position[0]) for p in other_positions]) < self.nearness_threshold
 
 def argmin(args, func):
     min_v = float('inf')
@@ -73,47 +82,102 @@ def dist(a, b):
 
 class Behavior:
     required_dims = 4
-    def __init__(self, response):
+    def __init__(self, response=[0]*4):
         self.response = response
         assert(len(response) == Behavior.required_dims)
 
     def react(self, cats, position):
-        return [0 for _ in range(len(position))]
+        return [-(position[1]-0.5), (position[0]-0.5)]
 
-def print_locations(cats, mice, killed):
-    print(f"cats:{cats}\nmice:{mice}\nkilled{killed}")
+class CatMouseSimulator:
+    def __init__(self, cat_locations, mice_locations, cat_behavior, plotter, plotter_complete=None):
+        self.cats = cat_locations
+        self.mice = mice_locations
+        self.cat_behavior = cat_behavior
+        self.plotter = plotter
+        self.plotter_complete = plotter_complete
+        self.killed=[]
 
-def simulate(num_cats, num_mice, cat_behavior, plotter = None):
+    """
+    Move the cats and mice each one step. Update the plotter, if any
+    Return true if the simulation should continue (i.e. mice are alive)
+    """
+    def iterate(self):
+        closest_mice = [argmin(self.mice, lambda mouse: dist(mouse.position[0], cat.position[0])) for cat in self.cats]
+        direction_to_mice = [[closest_mice[i][0].position[0][d] - cat.position[0][d] for d in range(len(cat.position[0]))] for i, cat in enumerate(self.cats)]
+        mice_moves = [self.cat_behavior.react(self.cats, self.mice[i].position[0]) for i in range(len(self.mice))]
+        # Move
+        for i, cat in enumerate(self.cats):
+            cat.accelerate_in_direction(direction_to_mice[i])
+        for i, p in enumerate(self.mice):
+            p.accelerate_in_direction(mice_moves[i])
+        # Kill
+        kill_indices = [self.mice[i].is_near(self.cats) for i in range(len(self.mice))]
+        self.killed.extend([self.mice[i] for i in range(len(self.mice)) if kill_indices[i]])
+        self.mice = [self.mice[i] for i in range(len(self.mice)) if not kill_indices[i]]
+        # Optionally, plot
+        if self.plotter:
+            self.plotter(self.cats, self.mice, self.killed)
+        if len(self.mice) != 0:
+            return True
+        if self.plotter_complete:
+            self.plotter_complete()
+        return False
+        
+
+def simulate_cat_mouse(num_cats, num_mice, cat_behavior, plotter = None, plotter_complete = None):
 
     cats = [Position() for _ in range(num_cats)]
     mice = [Position() for _ in range(num_mice)]
     killed = []
     iterations = 0
-    while(mice):
+    s = CatMouseSimulator(cats, mice, cat_behavior, plotter, plotter_complete)
+    while(s.iterate()):
         iterations += 1
-        closest_mice = [argmin(mice, lambda mouse: dist(mouse.position[0], cat.position[0])) for cat in cats]
-        direction_to_mice = [[closest_mice[i][0].position[0][d] - cat.position[0][d] for d in range(len(cat.position[0]))] for i, cat in enumerate(cats)]
-        mice_moves = [cat_behavior.react(cats, mice[i].position[0]) for i in range(len(mice))]
-        # Move
-        for i, cat in enumerate(cats):
-            cat.accelerate_in_direction(direction_to_mice[i])
-        for i, p in enumerate(mice):
-            p.accelerate_in_direction(mice_moves[i])
-        # Kill
-        kill_indices = [mice[i].is_near(cats) for i in range(len(mice))]
-        killed.extend([(mice[i], iterations) for i in range(len(mice)) if kill_indices[i]])
-        mice = [mice[i] for i in range(len(mice)) if not kill_indices[i]]
-        # Optionally, plot
-        if plotter:
-            plotter(cats, mice, killed)
-        print()
-        print()
     return iterations
 
+def print_locations(cats, mice, killed):
+    print(f"cats:{cats}\nmice:{mice}\nkilled{killed}")
+
+class CatMouseVisualizer:
+    def __init__(self, size=256, target="/Users/taylor/prey/test.gif"):
+        self.target = target
+        self.img_size = size
+        self.imgs = []
+    def visualize_locations(self, cats, mice, killed):
+        self.imgs.append(Image.new('RGB', (self.img_size, self.img_size), "black"))
+        draw = ImageDraw.Draw(self.imgs[-1])
+        dot_size = self.img_size*Position().nearness_threshold/2
+        def draw_at(pos, color):
+            draw.ellipse((pos[0]*self.img_size-dot_size, pos[1]*self.img_size-dot_size, pos[0]*self.img_size+dot_size, pos[1]*self.img_size+dot_size), fill=color)
+
+        for dead in killed:
+            draw_at(dead.position[0], "blue")
+        for mouse in mice:
+            draw_at(mouse.position[0], "white")
+        for cat in cats:
+            draw_at(cat.position[0], "red")
+    def complete(self):
+        print(f"Saving {len(self.imgs)} images into a gif")
+        Image.new('RGB', (self.img_size, self.img_size), "black").save(fp=self.target, format='GIF', append_images=self.imgs, save_all=True, duration=20, loop=0)
 
 def main():
-    iters = simulate(1, 3, Behavior([0]*4), print_locations)
+    vis = CatMouseVisualizer()
+    iters = simulate_cat_mouse(3, 50, Behavior([0]*4), vis.visualize_locations, vis.complete)
     print(f"Lived for {iters}")
     
 if __name__ == "__main__":
   main()
+
+## PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
+#img = Image.new( 'RGB', (250,250), "black") # create a new black image
+#pixels = img.load() # create the pixel map
+#
+#img.show()
+#index = 0
+#while True:
+#    index += 1
+#    for i in range(img.size[0]):    # for every col:
+#        for j in range(img.size[1]):    # For every row
+#            pixels[(i+index)%250,j] = (i, j, 100) # set the colour accordingly
+#    
---
+++ prey_test.py
@@ -0,0 +1,69 @@
+import unittest
+import prey
+from prey import Position
+
+def is_position_equal(pos, expected):
+    try:
+        # Not clear if we got coordinates or a position object, so try to extract coordinates
+        return is_position_equal(pos.position, expected)
+    except AttributeError:
+        pass
+    if not expected:
+        return True
+    try:
+        return all([is_position_equal(pos[i], expected[i]) for i in range(len(expected))])
+    except TypeError:
+        return pos == expected
+    raise ("Mismatch")
+
+class TestSimulation(unittest.TestCase):
+    def test_behavior_length_except(self):
+        with self.assertRaises(AssertionError):
+            p = prey.Behavior([])
+
+    def test_simulator_stops(self):
+        # With no mice, simulation should stop immediately
+        s = prey.CatMouseSimulator([], [], prey.Behavior(), None)
+        self.assertFalse(s.iterate())
+
+    def build_position_checker(self):
+        pass
+
+    def test_cat_acceleration(self):
+        """
+        Simulate one step, moving by velocity (2nd derivative, so position matrix is len 2)-
+        cat should accelerate one step to the mouse
+        """
+        def test_cat_from(init_position, plot_validator, should_mice_remain):
+            s = prey.CatMouseSimulator(
+                    [Position([init_position,[0,0]])],
+                    [Position([[0,0],[0,0]])],
+                    prey.Behavior(),
+                    plot_validator)
+            self.assertEqual(s.iterate(), should_mice_remain)
+        test_cat_from([0,0], None, False)
+
+
+class TestPosition(unittest.TestCase):
+    def test_move(self):
+        # Move with no momentum- each movement just updates velocity
+        p = Position([[0,0],[0,0]], 1, 0.1)
+        p.accelerate_in_direction([1,0])
+        self.assertTrue(is_position_equal(p, [[0.1, 0], [0.1, 0]]))
+        p.accelerate_in_direction([1,0])
+        self.assertTrue(is_position_equal(p, [[0.2, 0], [0.1, 0]]))
+        p.accelerate_in_direction([0,1])
+        self.assertTrue(is_position_equal(p, [[0.2, 0.1], [0, 0.1]]))
+
+    def test_bounce_velocity(self):
+        p = Position([[0,0],[0,0]], 1, 0.1)
+        p.accelerate_in_direction([-1, 0])
+        self.assertTrue(is_position_equal(p, [[0.1, 0], [-0.1, 0]]))
+
+    def test_bounce_accel(self):
+        p = Position([[0,0],[0,0],[0,0]], 1, 0.1, 0, [1.0, 1.0, 1.0])
+        p.accelerate_in_direction([-1, 0])
+        self.assertTrue(is_position_equal(p, [[0.1, 0], [0.1, 0], [-0.1, 0]]))
+
+if __name__ == '__main__':
+    unittest.main()
